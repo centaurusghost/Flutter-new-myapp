32663f643a200263fa8de91dd19e1a91cdfc48e9
Bishal <actionbishal98130@gmail.com>
2021-03-30 23:34:35 +0545

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(primarySwatch: Colors.blue),
-        home: LoginPage());
+        home: NextPage());
   }
 }
